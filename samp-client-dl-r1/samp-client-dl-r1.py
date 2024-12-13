e7b0722f866504fdce3271bd89405204b7064039
Calasans <127008180+ocalasans@users.noreply.github.com>
2024-12-13 19:52:16 -0300

    Källa Client - DL R1

--- samp-client-dl-r1/samp-client-dl-r1.py
+++ samp-client-dl-r1/samp-client-dl-r1.py
@@ -95,7 +95,7 @@ class Samp_Client_DL_R1:
         
         underrubrik = self.Skapa_Stiliserad_Etikett(
             huvud_ram, 
-            "Installationsprogram for mod SA:MP (San Andreas Multiplayer), version 0.3.7 DL R1"
+            "Installationsprogram for mod SA:MP (San Andreas Multiplayer), version 0.3 DL R1"
         )
         underrubrik.pack(pady=(0, 20))
         
