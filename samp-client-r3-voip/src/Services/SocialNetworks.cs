e79f2c5f4802814a14fbe3fe9f7a8a67e6cebc79
Calasans <127008180+ocalasans@users.noreply.github.com>
2024-12-23 02:46:15 -0300

    Source Code - Client - R3 SAMPVOICE

---
+++ samp-client-r3-voip/src/Services/SocialNetworks.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+namespace Client_R3_Voip_SPC.Services
+{
+    public class Social_Networks
+    {
+        public void OpenSocial_Network(string Network_Name)
+        {
+            string Network_Url = Network_Name switch
+            {
+                "Discord SPC" => "https://discord.gg/3fApZh66Tf",
+                "YouTube" => "https://youtube.com/@spc-samp",
+                "Instagram" => "https://instagram.com/spc.samp/",
+                "TikTok" => "https://tiktok.com/@spc.samp",
+                "GitHub" => "https://github.com/spc-samp",
+                _ => ""
+            };
+
+            if (!string.IsNullOrEmpty(Network_Url))
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Network_Url,
+                    UseShellExecute = true
+                });
+            }
+        }
+    }
+}
